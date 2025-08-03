9e6e11f8d769d098ffcc6773a2b22d624ed6f1b1
Daniel Caiafa <1450558+dcaiafa@users.noreply.github.com>
2025-08-03 11:09:17 -0700

    Raw Strings
    Support raw-strings like Go using backticks. Example:
    ```
    var a = `These will be interpreted literally: \n\t "foo"`
    ```

--- tests/string_test.go
+++ tests/string_test.go
@@ -7,6 +7,7 @@ func TestStringLiteral(t *testing.T) {
 	RunSubO(t, "simple_escape_seq", `print("Hello\n\r\t\\World")`, "Hello\n\r\t\\World")
 	RunSubO(t, "hex", `print("Hello\x2cWorld")`, "Hello,World")
 	RunSubO(t, "char", `print('A', '\n', '\'')`, `65 10 39`)
+	RunSubO(t, "raw", "print(`hello\\nworld`)", `hello\nworld`)
 }
 
 func TestFormattedString(t *testing.T) {
