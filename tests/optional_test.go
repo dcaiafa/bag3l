c88033bd19229c48292dd38b52d2697188f3f9cb
dcaiafa <1450558+dcaiafa@users.noreply.github.com>
2026-03-21 21:39:22 -0700

    fix: optional qualifier at end of line (#23)

--- tests/optional_test.go
+++ tests/optional_test.go
@@ -13,10 +13,14 @@ func TestOptional(t *testing.T) {
 		print(o.a.c[0].x?)
 		print(o.a.c[1].x?)
 		print(o.w.c[0].x?)
+
+		var x = o.a.c[0].x?
+		print(x)
 `, `
 1
 <nil>
 <nil>
+1
 `)
 	RunSubErr(t, "invalid1", `
 		var o = { 
